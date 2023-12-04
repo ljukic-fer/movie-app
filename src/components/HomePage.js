cfbcbc36d41bf251922ccb93e1d6c72d3a544060
ljukic-fer <71904233+ljukic-fer@users.noreply.github.com>
2023-12-04 02:23:58 +0100

    adjusted css

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react"; 
 import { Link } from 'react-router-dom';
+import './HomePage.css';
 
 const HomePage = () => {
     return (
-        <div>
+        <div className="blur-container">
             <h1>Welcome to the Movie App</h1>
             <p>Most popular movies of the day</p>
             <Link to='/movies'>
